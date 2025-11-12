04204fa24328f4f0d326971385fcfcd6d0dbd594
BRYAN.CALDERONESPINOZA <BRYAN.CALDERONESPINOZA@UCR.AC.CR>
2025-11-11 22:18:28 -0600

    CAMBIO

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrestamoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect('/prestamos/crear'));
+
+Route::get('/prestamos/crear', [PrestamoController::class, 'crear'])->name('prestamos.crear');
+Route::post('/prestamos/procesar', [PrestamoController::class, 'procesar'])->name('prestamos.procesar');
+Route::get('/prestamos/aprobados', [PrestamoController::class, 'aprobados'])->name('prestamos.aprobados');
